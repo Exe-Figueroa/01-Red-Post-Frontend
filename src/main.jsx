96468a2ebc827581c1488b5d8fbd30d2235221b1
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-07-05 16:50:08 -0300

    Se crea el comentario de ejemplo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './routes/App.jsx'
 import './index.css'
-
+//Hola soy el comentario
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
