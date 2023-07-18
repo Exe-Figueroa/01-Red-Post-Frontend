163b28e1d7b760f6fdbc4d4aaf37bf57f266f276
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-07-18 17:06:21 -0300

    Se crea la ruta del login y los estilos del formulario. Queda implementar lógica para jwt

---
+++ src/containers/Login.jsx
@@ -0,0 +1,33 @@
+import {React, useState} from 'react';
+import '../styles/Login.css';
+
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+
+export function Login() {
+  const [eye, setEye] = useState(true);
+  function toggleEye() {
+    setEye(!eye)
+  }
+  return (
+    <div className='Login'>
+      <form  action='' className="login-container">
+        <input type="text" placeholder='Username/Email' className='input'/>
+          
+        <div className="container-input-logo">
+          <input type="text" placeholder='Password' className='input'/>
+          {eye?
+          <FaEyeSlash className='eye' onClick={toggleEye}/>
+          :
+          <FaEye className='eye' onClick={toggleEye}/>
+          }
+        </div>
+
+        <div className="buttons-container">
+          <input type="submit" value="Login" className="login-btn"/>
+          <button className="register-btn">Register</button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,7 +2,8 @@ import {React, useState, useEffect} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {io} from 'socket.io-client'
 
-import {Home} from '../containers/Home.jsx'
+import {Home} from '../containers/Home.jsx';
+import {Login} from '../containers/Login.jsx';
 import { FooterMenu } from '../components/FooterMenu.jsx';
 
 const socket = io('http://localhost:3000')
@@ -21,6 +22,7 @@ function App() {
     <BrowserRouter>
     {connect? 'conectado': 'desconectado'}
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
       </Routes>
       <FooterMenu/>
